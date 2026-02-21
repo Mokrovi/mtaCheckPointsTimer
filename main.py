67e9e55d04dbb5180d70a778cd927f98a54a1124
anton <xxantonxx21@mail.ru>
2026-02-22 01:25:38 +0300

    в мта работает - за 3 проезда одинаковое количество чек поинтов

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ import mss
 import numpy
 
 def convertor(timestart, timecheck):
-    point = round((timecheck - timestart), 2)
-    if point // 60 == 0:
-        readble_time.append(repr(point).replace('.', ':'))
-    else:
-        minuts = repr(point // 60) + ":" + repr(point % 60).replace('.', ':')
-        readble_time.append(minuts)
+    point = round(timecheck - timestart, 2)
+    minutes = int(point // 60)
+    seconds = point % 60
+    return f"{minutes}:{seconds:.2f}".replace('.', ':')
 
 
 previouse_lap = []
@@ -27,18 +25,28 @@ while True:
         time.sleep(3)
         start_race_time = time.time()
         current_lap.append(start_race_time) # Первый чекпоинт старта
+        readble_checkpoint = convertor(current_lap[0], current_lap[0])
 
         sct = mss.mss()
 
         while True:
             img = numpy.asarray(sct.grab(mon))
 
-
             if find_point(img):
-                if time.time() - current_lap[-1] > 2:
-                    current_lap.append(time.time())
+                check_time = time.time()
+                if check_time - current_lap[-1] > 2:
+                    current_lap.append(check_time)
+
+                    readble_checkpoint = convertor(current_lap[0], check_time)
+                    readble_time.append(readble_checkpoint)
+
             if keyboard.is_pressed('f') & keyboard.is_pressed('alt'): # Конец гонки
                 break
-        print()
+
+        print(len(readble_time))
+        for j in range(0, len(readble_time)):
+            print(readble_time[j])
+
+
     if keyboard.is_pressed('ctrl') & keyboard.is_pressed('q'):
         break
